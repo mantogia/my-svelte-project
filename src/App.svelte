0bd2f2e4e0125b5b2491ce23c841627aec995838
mantogia <mantogia@students.zhaw.ch>
2022-10-30 23:18:53 +0100

    minute 37:30

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,37 @@
 <script>
 
+	import FeedbackList from "./components/FeedbackList.svelte";
+	let feedback = [
+        {
+            id: 1,
+            rating: 10,
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+        },
+        {
+            id: 2,
+            rating: 9,
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+        },
+        {
+            id: 3,
+            rating: 8,
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+        },
+        ]
+		
+	const deleteFeedback = (e) => {
+
+		console.log(e.detail)
+		const itemId = e.detail
+		feedback = feedback.filter((item) => item.id != itemId)
+	}
 
 </script>
 
-<main>
+<main class="container">
 
 	<h1>Hello!</h1>
-
+	<FeedbackList feedback={feedback} on:delete-feedback={deleteFeedback}/>
 </main>
 
 <style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,16 @@
+<script>
+    import FeedbackItem from "./FeedbackItem.svelte";
+    export let feedback = []
+    console.log(feedback)
+  
+
+
+</script>
+<main>
+    Feedback List
+
+    {#each feedback as fb (fb.id)} 
+       <FeedbackItem item = {fb} on:delete-feedback/>
+    {/each}
+
+</main>
